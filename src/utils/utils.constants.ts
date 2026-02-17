b189c05574fefb52d2ad6c780dc096752b444649
davmaene <davidmened@gmail.com>
2026-02-17 15:12:38 +0200

    up me line

--- src/utils/utils.constants.ts
+++ src/utils/utils.constants.ts
@@ -1,4 +1,4 @@
 export const APPNAME = "ULPGL - Goma"
 export const APPOWNER = "Université Libre des Pays des Grands Lacs"
 export const APPDNS = "https://ulpgl.net"
-export const APPCREATOR = "DNTIC - ULPGL"
+export const APPCREATOR = "Bureau de communication et de la DNTIC - ULPGL"
